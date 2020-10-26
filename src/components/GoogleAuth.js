eed96e24c3bbb07da4e55243f0343ded712b0d1e
Basil N G <basilng.bng@gmail.com>
2020-10-27 02:37:01 +0530

    Wiring up the Google API

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "482859178875-1dm9ehqvnbgo8igv4jvcgoambp0bff2e.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
