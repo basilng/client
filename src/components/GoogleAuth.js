cdddf3bfdcbaecb48e907c413a6b427662dc643e
Basil N G <basilng.bng@gmail.com>
2020-10-28 00:02:06 +0530

    Rendering buttons

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -26,11 +26,21 @@ class GoogleAuth extends React.Component {
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div> IDK</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>;
+      return (
+        <button className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div> I am not signed in</div>;
+      return (
+        <button className="ui green google button">
+          <i className="google icon" />
+          Sign In
+        </button>
+      );
     }
   }
 
