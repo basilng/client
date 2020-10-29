90c7cc4157a9f3e8e9fd51ff6d3d3337c415cbf5
Basil N G <basilng.bng@gmail.com>
2020-10-30 00:14:21 +0530

    Showing errors on page

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -16,7 +16,7 @@ class StreamCreate extends React.Component {
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} autoComplete="false" />
+        <input {...input} autoComplete="off" />
         {this.renderError(meta)}
       </div>
     );
@@ -30,7 +30,7 @@ class StreamCreate extends React.Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter title" />
         <Field
