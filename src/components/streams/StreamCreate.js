c001db8029d2a657648ca8d9988839692e88afbf
Basil N G <basilng.bng@gmail.com>
2020-10-30 19:42:49 +0530

    refactor connect

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
-
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
     if (touched && error) {
@@ -26,7 +25,7 @@ class StreamCreate extends React.Component {
   };
 
   onSubmit(formValues) {
-    console.log(formValues);
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -61,9 +60,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default connect()(
-  reduxForm({
-    form: "streamCreate",
-    validate,
-  })(StreamCreate)
-);
+const formWrapped = reduxForm({
+  form: "streamCreate",
+  validate,
+})(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
