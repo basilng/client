6401f18e7802034013b209c67f6cec529068da27
Basil N G <basilng.bng@gmail.com>
2020-10-31 01:28:33 +0530

    Rendering streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -6,13 +6,35 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>hi</h1>
+        <h2>Streams</h2>
+        <div className="ui called list">{this.renderList()}</div>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams),
+  };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
