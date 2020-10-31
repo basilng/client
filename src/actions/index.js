82bd21d28b2053d6c8fe1021c5359492a78b2a13
Basil N G <basilng.bng@gmail.com>
2020-10-31 22:43:07 +0530

    Creating a Browser History Object

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,6 +27,8 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+
+  //Do some programatic navigation to get the user back ti the root route
 };
 
 export const fetchStreams = () => async (dispatch) => {
