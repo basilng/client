3ecfb960ac440f1459505d8ddd1f53a4f08882e2
Basil N G <basilng.bng@gmail.com>
2020-11-01 18:59:23 +0530

    Making Modal resuable

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Modal from "../Modal";
 
+const actions = (
+  <div>
+    <button className="ui button negative">Delete</button>
+    <button className="ui button">Cancel</button>
+  </div>
+);
+
 const StreamDelete = () => {
   return (
     <div>
       Steam StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream"
+        actions={actions}
+      />
     </div>
   );
 };
