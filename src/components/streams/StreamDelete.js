0acb2f8d38c7c5a1ca19ca92ee480dd08c10c9ae
Basil N G <basilng.bng@gmail.com>
2020-11-01 20:54:40 +0530

    onDismiss custom function

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../..//history";
 
 const actions = (
-  <React.Fragment>
+  <>
     <button className="ui button negative">Delete</button>
     <button className="ui button">Cancel</button>
-  </React.Fragment>
+  </>
 );
 
 const StreamDelete = () => {
@@ -16,6 +17,7 @@ const StreamDelete = () => {
         title="Delete Stream"
         content="Are you sure you want to delete this stream"
         actions={actions}
+        onDismiss={() => history.push("/")}
       />
     </div>
   );
