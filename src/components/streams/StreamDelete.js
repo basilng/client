7c715d7a06f7beafc99d87f596ddb6f788e3bc27
Basil N G <basilng.bng@gmail.com>
2020-11-01 21:01:13 +0530

    Refactor streamDelete to class based component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,24 +2,29 @@ import React from "react";
 import Modal from "../Modal";
 import history from "../..//history";
 
-const actions = (
-  <>
-    <button className="ui button negative">Delete</button>
-    <button className="ui button">Cancel</button>
-  </>
-);
+class StreamDelete extends React.Component {
+  renderActions() {
+    return (
+      <>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </>
+    );
+  }
 
-const StreamDelete = () => {
-  return (
-    <div>
-      Steam StreamDelete
-      <Modal
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream"
-        actions={actions}
-        onDismiss={() => history.push("/")}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        Steam StreamDelete
+        <Modal
+          title="Delete Stream"
+          content="Are you sure you want to delete this stream"
+          actions={this.renderActions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
+}
+
 export default StreamDelete;
