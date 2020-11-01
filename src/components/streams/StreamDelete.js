c86c6a67b88af490e4093e4642e5f3f43a024006
Basil N G <basilng.bng@gmail.com>
2020-11-01 21:07:33 +0530

    wiring up actions to streamDelete

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
 import Modal from "../Modal";
-import history from "../..//history";
+import history from "../../history";
+import { fetchStream, deleteStream } from "../../actions";
 
 class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
   renderActions() {
     return (
       <>
@@ -27,4 +33,4 @@ class StreamDelete extends React.Component {
   }
 }
 
-export default StreamDelete;
+export default connect(null, { fetchStream, deleteStream })(StreamDelete);
